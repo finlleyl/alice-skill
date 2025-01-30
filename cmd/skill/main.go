e036e708c0f3a2de8ff18dc2580ccffb0ae640d2
finlleyl <ilya_chibugaev@mail.ru>
2025-01-30 11:13:16 +0400

    added gzip support

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/finlleyl/alice-skill/internal/logger"
+	"github.com/finlleyl/alice-skill/internal/models"
 	"go.uber.org/zap"
 	"net/http"
+	"strings"
 )
 
 func main() {
@@ -19,8 +22,7 @@ func run() error {
 	}
 
 	logger.Log.Info("Running server", zap.String("address", flagRunAddr))
-
-	return http.ListenAndServe(flagRunAddr, logger.RequestLogger(webhook))
+	return http.ListenAndServe(flagRunAddr, logger.RequestLogger(gzipMiddleware(webhook)))
 }
 
 func webhook(w http.ResponseWriter, r *http.Request) {
@@ -30,14 +32,61 @@ func webhook(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	logger.Log.Debug("decoding request")
+	var req models.Request
+	dec := json.NewDecoder(r.Body)
+	if err := dec.Decode(&req); err != nil {
+		logger.Log.Debug("cannot decode request JSON body", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if req.Request.Type != models.TypeSimpleUtterance {
+		logger.Log.Debug("unsupported request type", zap.String("type", req.Request.Type))
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		return
+	}
+
+	resp := models.Response{
+		Response: models.ResponsePayload{
+			Text: "Извините, я пока ничего не умею",
+		},
+		Version: "1.0",
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	_, _ = w.Write([]byte(`
-          {
-            "response": {
-              "text": "Извините, я пока ничего не умею"
-            },
-            "version": "1.0"
-          }
-        `))
+
+	enc := json.NewEncoder(w)
+	if err := enc.Encode(resp); err != nil {
+		logger.Log.Debug("error encoding response", zap.Error(err))
+		return
+	}
 	logger.Log.Debug("sending HTTP 200 response")
+}
+
+func gzipMiddleware(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ow := w
+
+		acceptEncoding := r.Header.Get("Accept-Encoding")
+		supportsGzip := strings.Contains(acceptEncoding, "gzip")
+		if supportsGzip {
+			cw := newCompressWriter(w)
+			ow = cw
+			defer cw.Close()
+		}
+
+		contentEncoding := r.Header.Get("Content-Encoding")
+		sendsGzip := strings.Contains(contentEncoding, "gzip")
+		if sendsGzip {
+			cr, err := newCompressReader(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			r.Body = cr
+			defer cr.Close()
+		}
+		h.ServeHTTP(ow, r)
+	}
 }
--- cmd/skill/main_test.go
+++ cmd/skill/main_test.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	"github.com/go-resty/resty/v2"
-	"github.com/stretchr/testify/assert"
+	"bytes"
+	"compress/gzip"
+	"github.com/stretchr/testify/require"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestWebhook(t *testing.T) {
@@ -14,21 +19,57 @@ func TestWebhook(t *testing.T) {
 	defer srv.Close()
 
 	successBody := `{
-		"response": {
-		"text": "Извините, я пока ничего не умею"
-		},
-		"version: "1.0"
-	}`
+        "response": {
+            "text": "Извините, я пока ничего не умею"
+        },
+        "version": "1.0"
+    }`
 
 	testCases := []struct {
+		name         string
 		method       string
+		body         string
 		expectedCode int
 		expectedBody string
 	}{
-		{method: http.MethodGet, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
-		{method: http.MethodPut, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
-		{method: http.MethodDelete, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
-		{method: http.MethodPost, expectedCode: http.StatusOK, expectedBody: successBody},
+		{
+			name:         "method_get",
+			method:       http.MethodGet,
+			expectedCode: http.StatusMethodNotAllowed,
+			expectedBody: "",
+		},
+		{
+			name:         "method_put",
+			method:       http.MethodPut,
+			expectedCode: http.StatusMethodNotAllowed,
+			expectedBody: "",
+		},
+		{
+			name:         "method_delete",
+			method:       http.MethodDelete,
+			expectedCode: http.StatusMethodNotAllowed,
+			expectedBody: "",
+		},
+		{
+			name:         "method_post_without_body",
+			method:       http.MethodPost,
+			expectedCode: http.StatusInternalServerError,
+			expectedBody: "",
+		},
+		{
+			name:         "method_post_unsupported_type",
+			method:       http.MethodPost,
+			body:         `{"request": {"type": "idunno", "command": "do something"}, "version": "1.0"}`,
+			expectedCode: http.StatusUnprocessableEntity,
+			expectedBody: "",
+		},
+		{
+			name:         "method_post_success",
+			method:       http.MethodPost,
+			body:         `{"request": {"type": "SimpleUtterance", "command": "sudo do something"}, "version": "1.0"}`,
+			expectedCode: http.StatusOK,
+			expectedBody: successBody,
+		},
 	}
 
 	for _, tc := range testCases {
@@ -37,13 +78,84 @@ func TestWebhook(t *testing.T) {
 			req.Method = tc.method
 			req.URL = srv.URL
 
+			if len(tc.body) > 0 {
+				req.SetHeader("Content-Type", "application/json")
+				req.SetBody(tc.body)
+			}
+
 			resp, err := req.Send()
 			assert.NoError(t, err, "error making HTTP request")
-			assert.Equal(t, tc.expectedCode, resp.StatusCode(), "Response code didn`t match expected")
+
+			assert.Equal(t, tc.expectedCode, resp.StatusCode(), "Response code didn't match expected")
 			if tc.expectedBody != "" {
-				assert.Equal(t, tc.expectedBody, string(resp.Body()))
+				assert.JSONEq(t, tc.expectedBody, string(resp.Body()))
 			}
 		})
 	}
+}
 
+func TestGzipCompression(t *testing.T) {
+	handler := http.HandlerFunc(gzipMiddleware(webhook))
+
+	srv := httptest.NewServer(handler)
+	defer srv.Close()
+
+	requestBody := `{
+        "request": {
+            "type": "SimpleUtterance",
+            "command": "sudo do something"
+        },
+        "version": "1.0"
+    }`
+
+	successBody := `{
+        "response": {
+            "text": "Извините, я пока ничего не умею"
+        },
+        "version": "1.0"
+    }`
+
+	t.Run("sends_gzip", func(t *testing.T) {
+		buf := bytes.NewBuffer(nil)
+		zb := gzip.NewWriter(buf)
+		_, err := zb.Write([]byte(requestBody))
+		require.NoError(t, err)
+		err = zb.Close()
+		require.NoError(t, err)
+
+		r := httptest.NewRequest("POST", srv.URL, buf)
+		r.RequestURI = ""
+		r.Header.Set("Content-Encoding", "gzip")
+
+		resp, err := http.DefaultClient.Do(r)
+		require.NoError(t, err)
+		require.Equal(t, http.StatusOK, resp.StatusCode)
+
+		defer resp.Body.Close()
+
+		b, err := io.ReadAll(resp.Body)
+		require.NoError(t, err)
+		require.JSONEq(t, successBody, string(b))
+	})
+
+	t.Run("accepts_gzip", func(t *testing.T) {
+		buf := bytes.NewBufferString(requestBody)
+		r := httptest.NewRequest("POST", srv.URL, buf)
+		r.RequestURI = ""
+		r.Header.Set("Accept-Encoding", "gzip")
+
+		resp, err := http.DefaultClient.Do(r)
+		require.NoError(t, err)
+		require.Equal(t, http.StatusOK, resp.StatusCode)
+
+		defer resp.Body.Close()
+
+		zr, err := gzip.NewReader(resp.Body)
+		require.NoError(t, err)
+
+		b, err := io.ReadAll(zr)
+		require.NoError(t, err)
+
+		require.JSONEq(t, successBody, string(b))
+	})
 }
